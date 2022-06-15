7e0cf524ee8aa99bab129b77ddfe155a7760f59e
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-06-15 15:58:32 +1000

    Added initial ToyRobotTest.App project.

---
+++ ToyRobotTest.App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ToyRobotTest.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Toy Robot Test";
+
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
