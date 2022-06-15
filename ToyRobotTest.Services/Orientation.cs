708f55d0e15cdb26675bb755c0dc0249ff0ea0a7
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-06-15 16:34:24 +1000

    Added ToyRobot class.

---
+++ ToyRobotTest.Services/Orientation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobotTest.Services
+{
+    /// <summary>
+    /// The possible orientations for a toy robot.
+    /// </summary>
+    public enum Orientation
+    {
+        North,
+        South,
+        East,
+        West
+    }
+}
--- ToyRobotTest.Services/ToyRobot.cs
+++ ToyRobotTest.Services/ToyRobot.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace ToyRobotTest.Services
 {
+    /// <summary>
+    /// A toy robot that can be moved on a table-top.
+    /// </summary>
     public class ToyRobot
     {
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+        public bool IsPlacedOnTable { get; set; }
+        public Orientation Orientation { get; set; }
     }
 }
