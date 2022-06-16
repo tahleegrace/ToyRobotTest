e77dee8c2b7dc53c216b56f0d9c8fbda4703c582
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-06-16 15:04:08 +1000

    Added unit tests project.

---
+++ ToyRobotTest.Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ToyRobotTest.Services.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ToyRobotTest.Services.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6e9956eb-df3a-4052-99dd-fabbfedc9d20")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ToyRobotTest.Services.Tests/ToyRobotTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ToyRobotTest.Services.Tests
+{
+    [TestClass]
+    public class ToyRobotTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
